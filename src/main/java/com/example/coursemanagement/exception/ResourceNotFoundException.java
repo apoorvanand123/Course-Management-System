bd1601437338fb40b795638a77a6d1f028f98f2c
Apoorv Anand <118161881+apoorvanand123@users.noreply.github.com>
2025-03-31 11:03:14 +0530

    Update ResourceNotFoundException.java

--- src/main/java/com/example/coursemanagement/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/coursemanagement/exception/ResourceNotFoundException.java
@@ -1,1 +1,7 @@
+package com.example.coursemanagement.exception;
 
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
