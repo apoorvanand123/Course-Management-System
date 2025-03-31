1a8a8ecce2790c4d8477d50748a948a8b59fa9f1
Apoorv Anand <118161881+apoorvanand123@users.noreply.github.com>
2025-03-31 10:59:28 +0530

    StudentController.java

--- src/main/java/com/example/coursemanagement/controller/StudentController.java
+++ src/main/java/com/example/coursemanagement/controller/StudentController.java
@@ -1,1 +1,31 @@
+package com.example.coursemanagement.controller;
 
+import com.example.coursemanagement.model.Student;
+import com.example.coursemanagement.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+
+    @GetMapping("/{studentId}")
+    public Student getStudent(@PathVariable String studentId) {
+        return studentService.getStudent(studentId);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+}
