1a2710d98f9c58ffd5e15f1cbd49b67276920d12
Apoorv Anand <118161881+apoorvanand123@users.noreply.github.com>
2025-03-31 11:04:26 +0530

    Update Student.java

--- src/main/java/com/example/coursemanagement/model/Student.java
+++ src/main/java/com/example/coursemanagement/model/Student.java
@@ -1,1 +1,34 @@
+package com.example.coursemanagement.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    private String studentId;
+    private String studentName;
+
+    // Getters and setters
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    @Override
+    public String toString() {
+        return studentName + " (" + studentId + ")";
+    }
+}
