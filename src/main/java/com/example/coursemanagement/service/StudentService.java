36386ca645b4c85e57eb8d594d2a0fd278457077
Apoorv Anand <118161881+apoorvanand123@users.noreply.github.com>
2025-03-31 11:06:34 +0530

    Update StudentService.java

--- src/main/java/com/example/coursemanagement/service/StudentService.java
+++ src/main/java/com/example/coursemanagement/service/StudentService.java
@@ -1,1 +1,28 @@
+package com.example.coursemanagement.service;
 
+import com.example.coursemanagement.exception.ResourceNotFoundException;
+import com.example.coursemanagement.model.Student;
+import com.example.coursemanagement.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student getStudent(String studentId) {
+        return studentRepository.findById(studentId).orElseThrow(() -> new ResourceNotFoundException("Student not found"));
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
